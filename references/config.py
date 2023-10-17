43705c45a18e2606ce0a573e1a05f195fb5c4888
mykytaalekseiev <alekseyevnf@gmail.com>
2023-10-17 20:01:15 +0200

    Small refactoring.

--- references/config.py
+++ references/config.py
@@ -1,4 +1,3 @@
-
 import datetime as dt
 
 DATES = {"train_start": dt.datetime(2015, 6, 30), 
@@ -7,5 +6,3 @@ DATES = {"train_start": dt.datetime(2015, 6, 30),
          "val_end": dt.datetime(2019, 1, 31),
          "infer_start": dt.datetime(2019, 1, 31), 
          "infer_end": dt.datetime(2022, 1, 31)}
-
-
