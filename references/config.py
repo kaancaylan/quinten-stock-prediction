79a5d315168f0c01b9acd19a65f62aac7e01a9dd
guillaume <adherouville@outlook.com>
2023-10-17 13:06:46 +0200

    .DS_Store ignore + config file

---
+++ references/config.py
@@ -0,0 +1,11 @@
+
+import datetime as dt
+
+DATES = {"train_start": dt.datetime(2015, 6, 30), 
+         "train_end": dt.datetime(2018, 6, 30),
+         "val_start": dt.datetime(2018, 6, 30), 
+         "val_end": dt.datetime(2019, 1, 31),
+         "infer_start": dt.datetime(2019, 1, 31), 
+         "infer_end": dt.datetime(2022, 1, 31)}
+
+
