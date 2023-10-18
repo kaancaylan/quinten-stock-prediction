f78ddf784a9a2666905819fcff81165eea8e78a4
Kaan <kaan@skypher.co>
2023-10-18 19:22:51 +0200

    Code reformatting

--- src/data/preprocessing_utils.py
+++ src/data/preprocessing_utils.py
@@ -14,7 +14,7 @@ def get_data(path="raw_data_finance.csv"):
     df = df[df["close"].notna()]
 
     # Drop duplicates on index
-    df = df[~df.index.duplicated(keep='first')]
+    df = df[~df.index.duplicated(keep="first")]
 
     # convert all values into numeric
     for col in df.select_dtypes(include="object").columns:
@@ -22,14 +22,18 @@ def get_data(path="raw_data_finance.csv"):
             df[col] = pd.to_numeric(df[col])
         except:
             pass
-    
+
     # Drop all stocks with less than 1000 volume
-    df = df[df["volume"]>1000]
+    df = df[df["volume"] > 1000]
 
     # Outlier columns that cannot be auto handles
     irrelevant_cols = ["cik", "finalLink", "link"]
 
-    df["period"] = df["period"].replace("FY", "Q4").apply(lambda x: int(x[-1]) if isinstance(x, str) else x)
+    df["period"] = (
+        df["period"]
+        .replace("FY", "Q4")
+        .apply(lambda x: int(x[-1]) if isinstance(x, str) else x)
+    )
 
     # Analyst Recommendations handling
 
@@ -38,7 +42,7 @@ def get_data(path="raw_data_finance.csv"):
         "Buy": 1,
         "Neutral": 0,
         "Sell": -1,
-        "Strong Sell": -2
+        "Strong Sell": -2,
     }
     recommendation_cols = df.filter(regex="Recommendation").columns
     for col in recommendation_cols:
@@ -46,36 +50,38 @@ def get_data(path="raw_data_finance.csv"):
     df = add_return(df)
     df = feature_engineering(df)
     df.replace([np.inf, -np.inf], np.nan, inplace=True)
-    
 
     return df.drop(columns=irrelevant_cols).sort_index(level=0)
 
 
 def add_return(df):
-    df["return"] = df["close"].groupby(level="symbol", group_keys=False).apply(lambda x: x.pct_change())
+    df["return"] = (
+        df["close"]
+        .groupby(level="symbol", group_keys=False)
+        .apply(lambda x: x.pct_change())
+    )
     return df
 
 
 def calculate_eps(net_income, weighted_average_shs_out):
     # Use the .replace method to replace 0 with NaN to avoid division by zero
-    weighted_average_shs_out_replaced = weighted_average_shs_out.replace(
-        0, np.nan)
+    weighted_average_shs_out_replaced = weighted_average_shs_out.replace(0, np.nan)
     # Now perform element-wise division
     eps = net_income / weighted_average_shs_out_replaced
     return eps
 
 
 def feature_engineering(df):
-    df['ROI'] = (df['capitalExpenditure'] / df['netIncome']) * 100
-    df['revenue'] = df['revenue'].replace(0, np.nan)
-    df['profit_margin'] = (df['netIncome'] / df['revenue']) * 100
-    df['weightedAverageShsOut'] = df['weightedAverageShsOut'].replace(
-        0, np.nan)
-    eps = calculate_eps(df['netIncome'], df['weightedAverageShsOut'])
-    df['eps'] = eps
-    df['eps'] = df['eps'].replace(0, np.nan)
-    df['market_value_per_share'] = df['marketCapitalization'] / \
-                                   df['weightedAverageShsOut']
-    df['pe_ratio'] = df['market_value_per_share'] / df['eps']
+    df["ROI"] = (df["capitalExpenditure"] / df["netIncome"]) * 100
+    df["revenue"] = df["revenue"].replace(0, np.nan)
+    df["profit_margin"] = (df["netIncome"] / df["revenue"]) * 100
+    df["weightedAverageShsOut"] = df["weightedAverageShsOut"].replace(0, np.nan)
+    eps = calculate_eps(df["netIncome"], df["weightedAverageShsOut"])
+    df["eps"] = eps
+    df["eps"] = df["eps"].replace(0, np.nan)
+    df["market_value_per_share"] = (
+        df["marketCapitalization"] / df["weightedAverageShsOut"]
+    )
+    df["pe_ratio"] = df["market_value_per_share"] / df["eps"]
 
     return df
--- src/models/train.py
+++ src/models/train.py
@@ -8,12 +8,14 @@ from sklearn.model_selection import HalvingGridSearchCV
 from sklearn.model_selection import TimeSeriesSplit
 
 
-
-
 def train(
-        training_start: dt.datetime, training_end: dt.datetime,
-        validation_start: dt.datetime, validation_end: dt.datetime,
-        model_name: str = "Model1", df=None, hyperparam_tuning=False,
+    training_start: dt.datetime,
+    training_end: dt.datetime,
+    validation_start: dt.datetime,
+    validation_end: dt.datetime,
+    model_name: str = "Model1",
+    df=None,
+    hyperparam_tuning=False,
 ):
     """Train the model based on a given set of training and validation dates.
         All the date values must exactly match the ones in the dataset.
@@ -21,7 +23,7 @@ def train(
     Args:
         training_start (dt.datetime): The date in which training period will start
         training_end (dt.datetime): The date in which the training period will end
-        validation_start (dt.datetime): The date in which the validation period starts. Validation period is used 
+        validation_start (dt.datetime): The date in which the validation period starts. Validation period is used
                                     as the label for the training data. The period is usually taken as 6 months.
         validation_end (dt.datetime): The date in which the validation period will end.
         model_name (str, optional): the name for the model for saving purposes. Defaults to "Model1".
@@ -29,14 +31,14 @@ def train(
     """
     if df is None:
         df = pr.get_data()
-    X_train = df.loc[training_start: training_end]
-    if len(X_train)==0:
+    X_train = df.loc[training_start:training_end]
+    if len(X_train) == 0:
         return None
     X_train = X_train.unstack(level=0).drop("year", axis=1)
     X_train = X_train.select_dtypes(exclude=["object", "datetime"])
 
     # Testing data
-    test_per_rets = df.loc[validation_start: validation_end, "return"]
+    test_per_rets = df.loc[validation_start:validation_end, "return"]
 
     X_train, y_train = modeling_prep(X_train, test_per_rets)
     model = XGBRegressor()
@@ -44,25 +46,32 @@ def train(
     if hyperparam_tuning:
         tscv = TimeSeriesSplit(n_splits=3)
         param_grid = {
-            'max_depth': [3, 5, 7],
-            'reg_lambda': [0.1, 1, 2],
-            'n_estimators': [50, 100, 150],
-            'reg_alpha': [0.1, 1 ,2]
-            }
-        grid_search = HalvingGridSearchCV(model, param_grid, cv=tscv,  n_jobs=4)
+            "max_depth": [3, 5, 7],
+            "reg_lambda": [0.1, 1, 2],
+            "n_estimators": [50, 100, 150],
+            "reg_alpha": [0.1, 1, 2],
+        }
+        grid_search = HalvingGridSearchCV(model, param_grid, cv=tscv, n_jobs=4)
         return grid_search.best_estimator_
 
     model.fit(X_train.to_numpy(), y_train)
     model.save_model(f"src/models/{model_name}")
     return model
-    
+
 
 def modeling_prep(X_train, test_per_rets):
     # drop stocks with missing returns for this period
-    to_drop = test_per_rets[test_per_rets.isna()].index.get_level_values("symbol").unique()
+    to_drop = (
+        test_per_rets[test_per_rets.isna()].index.get_level_values("symbol").unique()
+    )
 
     # drop stocks that traded under 1$ in the period
-    under_1 = X_train['close'].groupby(level="symbol").apply(lambda x: (x < 1).any(axis=1)).droplevel(1)
+    under_1 = (
+        X_train["close"]
+        .groupby(level="symbol")
+        .apply(lambda x: (x < 1).any(axis=1))
+        .droplevel(1)
+    )
     to_drop = to_drop.union(under_1[under_1].index).unique()
     to_drop = to_drop[to_drop.isin(X_train.index)]
 
@@ -73,9 +82,10 @@ def modeling_prep(X_train, test_per_rets):
     drop_from_y = np.setdiff1d(y_train.index.get_level_values(1), X_train.index)
     X_train = X_train.drop(drop_from_x)
     y_train = y_train.drop(drop_from_y, level=1)
-    y_train = y_train.groupby(level=1, group_keys=False).apply(lambda x: (1+x).cumprod()[-1]-1)
-    
-   
+    y_train = y_train.groupby(level=1, group_keys=False).apply(
+        lambda x: (1 + x).cumprod()[-1] - 1
+    )
+
     assert all(X_train.index == y_train.index)
 
     X_train = X_train.select_dtypes(exclude=["object", "datetime"])
@@ -94,16 +104,24 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train the model")
-    parser.add_argument("--training_start",
-                        help="Date for the start of training. Format should be YYY/MM/DD, the day should be the last "
-                             "day of month. ",
-                        type=str)
-    parser.add_argument("--training_end",
-                        help="Date for the end of training. Format should be YYY/MM/DD, the day should be the last "
-                             "day of month. ",
-                        type=str)
-    parser.add_argument("--validation_start", help="Date for the start of validation period", type=str)
-    parser.add_argument("--validation_end", help="Date for the start of validation period", type=str)
+    parser.add_argument(
+        "--training_start",
+        help="Date for the start of training. Format should be YYY/MM/DD, the day should be the last "
+        "day of month. ",
+        type=str,
+    )
+    parser.add_argument(
+        "--training_end",
+        help="Date for the end of training. Format should be YYY/MM/DD, the day should be the last "
+        "day of month. ",
+        type=str,
+    )
+    parser.add_argument(
+        "--validation_start", help="Date for the start of validation period", type=str
+    )
+    parser.add_argument(
+        "--validation_end", help="Date for the start of validation period", type=str
+    )
     parser.add_argument("--model_name", type=str)
 
     args = parser.parse_args()
